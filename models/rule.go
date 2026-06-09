16e0fe16173c0358c3a09f8bfac3761b3de65166
jordan home lab <jordan-home-lab@example.com>
2026-06-09 09:20:42 -0600

    allocations

--- models/rule.go
+++ models/rule.go
@@ -1,9 +1,15 @@
 package models
 
 type Rule struct {
-	ID       uint    `gorm:"primaryKey" json:"id"`
-	Priority int     `json:"priority"`
-	Amount   float64 `json:"amount"`
-	BucketID uint    `json:"bucket_id"`
-	Bucket   Bucket  `gorm:"foreignKey:BucketID" json:"bucket"`
+	ID          uint                     `gorm:"primaryKey" json:"id"`
+	Priority    int                      `json:"priority"`
+	Amount      float64                  `json:"amount"`
+	Allocations []ProportionalAllocation `gorm:"embedded" json:"allocation"`
+	BucketID    uint                     `json:"bucket_id"`
+	Bucket      Bucket                   `gorm:"foreignKey:BucketID" json:"bucket"`
+}
+
+type ProportionalAllocation struct {
+	Percentage float64 `json:"percentage"`
+	Limit      float64 `json:"limit"`
 }
